cc34c0a4a159a5ec1de89dc6961266b9d7e1be36
PBJTP <jplypick1990@gmail.com>
2023-02-27 23:03:43 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//create a LinkedList factory the is the full list
+
+//create factory function that will create a node containing data and pointer to next node. both null by default
+
+/////////// add prototypes to function that do the following: ////////////
+//append a new node to end of list
+//prepend a value to the start of the list
+//return the size of the list
+//return the head of the list
+//return the tail of the list
+//return the node at a given index
+//pop the last node from the list
+//return true if a node contains a given value, else false
+//return the index of the node containing given value
+//convert objects data to a string and display in console
+
+//EXTRA CREDIT
+//insert a new node at a given index (value, index)
+//remove a node at a given index
+
+function LinkedList() {
+    return 'stuff';
+};
+
+function Node(data) {
+    return {
+        data: data,
+        next: null,
+    }
+};
